f0385d57691963ca355f5659b49671ef1d645f6d
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-11 15:57:54 -0300

    trabalhando com o DTO

--- src/main/java/com/dscatalog/service/CategoryService.java
+++ src/main/java/com/dscatalog/service/CategoryService.java
@@ -1,12 +1,14 @@
 package com.dscatalog.service;
 
-import java.util.List;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.model.Category;
 import com.dscatalog.repository.CategoryRepository;
 
@@ -20,7 +22,10 @@ public class CategoryService {
 	
 	@Transactional(readOnly = true)
 	@GetMapping(value = "")
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category>list = categoryRepository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
