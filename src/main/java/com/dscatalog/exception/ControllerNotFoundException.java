8d58b05020b6c1f7bba4e29e495090fa4982c839
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-12 20:50:00 -0300

    mudando o nome da classe de NotFound

---
+++ src/main/java/com/dscatalog/exception/ControllerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.dscatalog.exception;
+
+public class ControllerNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ControllerNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
