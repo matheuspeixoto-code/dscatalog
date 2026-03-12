aa5f4e474c907f1d514d21cc958a5a9ae7693e15
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-12 19:06:04 -0300

    trabalhando com exception e nova rota de post

--- src/main/java/com/dscatalog/controller/CategoryController.java
+++ src/main/java/com/dscatalog/controller/CategoryController.java
@@ -1,12 +1,18 @@
 package com.dscatalog.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.model.Category;
@@ -24,5 +30,20 @@ public class CategoryController {
 		List<CategoryDTO> list = categoryService.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable("id") Long id){
+		CategoryDTO dto = categoryService.findById(id);
+		return ResponseEntity.ok().body(dto);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto= categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
 	}
 }
