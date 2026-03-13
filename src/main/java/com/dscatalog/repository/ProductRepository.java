c858bafa1caf6ebb9c84d1e218160f22afb06230
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-13 16:13:35 -0300

    ProductRepository

---
+++ src/main/java/com/dscatalog/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.dscatalog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dscatalog.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
