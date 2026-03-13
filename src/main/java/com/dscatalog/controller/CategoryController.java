645f5fa3b84fe1cc82b5c02af7a0e90fbb373747
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-12 21:01:14 -0300

    rota de delete

--- src/main/java/com/dscatalog/controller/CategoryController.java
+++ src/main/java/com/dscatalog/controller/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,15 @@ public class CategoryController {
 		return ResponseEntity.ok().body(dto);
 		
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> delete(@PathVariable("id") Long id  ){
+		CategoryDTO dto = categoryService.delete(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	
+	
+	
 }
