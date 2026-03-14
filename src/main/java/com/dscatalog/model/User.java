f89c3511a55c592ed067df5e252e932d5a07259d
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-14 15:30:09 -0300

    fazendo o fetch em user

--- src/main/java/com/dscatalog/model/User.java
+++ src/main/java/com/dscatalog/model/User.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,7 +35,7 @@ public class User implements Serializable{
 	
 	private String password;
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "tb_user_role",
 			joinColumns = @JoinColumn(name ="user_id"),
