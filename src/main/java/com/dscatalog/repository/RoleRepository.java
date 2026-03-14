77452db603d7335a5ff4f4e8d90c3e7f60a4f86d
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-14 15:31:25 -0300

    RoleRepository

---
+++ src/main/java/com/dscatalog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.dscatalog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dscatalog.model.Role;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
