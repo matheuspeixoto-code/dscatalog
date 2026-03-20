d1b9c5c787024b5d299342c1fc4ab739abba2788
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-20 16:40:45 -0300

    inserindo a lista da errors

--- src/main/java/com/dscatalog/exception/ControllerExceptionHandle.java
+++ src/main/java/com/dscatalog/exception/ControllerExceptionHandle.java
@@ -4,11 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.dscatalog.controller.exceptions.StandardError;
+import com.dscatalog.controller.exceptions.ValidationError;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -28,14 +30,18 @@ public class ControllerExceptionHandle {
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError>validation(MethodArgumentNotValidException e, HttpServletRequest request){
-		StandardError err= new StandardError();
+	public ResponseEntity<ValidationError>validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err= new ValidationError();
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
 		err.setError("Validation exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		
+		for (FieldError f  : e.getBindingResult().getFieldErrors()) {
+			err.addErrros( f.getField() , f.getDefaultMessage() );
+		}
 		
 		return ResponseEntity.status(status).body(err);
 	}
