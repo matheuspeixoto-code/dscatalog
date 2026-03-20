a75d2e1ba281f064949559b56954199c6beb83ca
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-03-20 16:26:08 -0300

    ExceptionHandler MethodArgumentNotValidException

--- src/main/java/com/dscatalog/exception/ControllerExceptionHandle.java
+++ src/main/java/com/dscatalog/exception/ControllerExceptionHandle.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,5 +25,18 @@ public class ControllerExceptionHandle {
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError>validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		StandardError err= new StandardError();
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
 	}
 }
